fc929838c50b258235cf017952a008c763df343b
Horquidia <horquidia.a.lima@gmail.com>
2024-04-09 16:43:07 -0300

    Extensão de interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
-import { Post } from "./components/Post"
+import { Post, PostType } from "./components/Post"
 
 import styles from './App.module.css'
 import './global.css'
 
-const posts = [
+
+const posts: PostType[] = [
   {
     id:1,
     author: {
@@ -16,7 +17,7 @@ const posts = [
     content: [
       {type: 'paragraph', content: 'Fala galeraa 👋'},
       {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      {type: 'link', conttent: 'jane.design/doctorcare'},
+      {type: 'link', content: 'jane.design/doctorcare'},
     ],
     publishedAt: new Date('2024-04-01 20:00:00'),
   },
@@ -50,9 +51,7 @@ export function App() {
             return(
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post= {post}
               />
             )
           })}
